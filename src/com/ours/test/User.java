52635baa31ef0f0e5e4a456b22bcbc952a5d2117
18206028315 <1137487514@qq.com>
2020-12-05 10:43:47 +0800

    第一次提交

---
+++ src/com/ours/test/User.java
@@ -0,0 +1,18 @@
+package com.ours.test;
+
+/**
+ * 作者：凌宇
+ * 日期：2020/12/5 10:39
+ * 描述：
+ */
+public class User {
+    private String User;
+
+    public String getUser() {
+        return User;
+    }
+
+    public void setUser(String user) {
+        User = user;
+    }
+}
